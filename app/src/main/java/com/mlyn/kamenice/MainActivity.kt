b32c4648e9d631d70b16fa0a8442625c014dd395
Oleg Yapparov <Oleg.Yapparov@infor.com>
2021-11-16 16:53:48 +0100

    Update kotlin plugin, add Apollo dependency, make first GraphQL call

--- app/src/main/java/com/mlyn/kamenice/MainActivity.kt
+++ app/src/main/java/com/mlyn/kamenice/MainActivity.kt
@@ -2,12 +2,20 @@ package com.mlyn.kamenice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.CalendarView
 import android.widget.Toast
+import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var calendarView: CalendarView
+    private val apolloClient: ApolloClient = ApolloClient.builder()
+        .serverUrl("https://kamenice.pythonanywhere.com/api")
+        .build()
 
     // https://github.com/AppliKeySolutions/CosmoCalendar
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,12 +23,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         calendarView = findViewById(R.id.reservationsCalendar)
-        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
             Toast.makeText(
                 this@MainActivity,
                 "Year $year, month $month, day $dayOfMonth clicked",
                 Toast.LENGTH_SHORT
             ).show()
+        }
+
+        try {
+            apolloClient.query(SettingsQuery()).enqueue(
+                object : ApolloCall.Callback<SettingsQuery.Data>() {
+                    override fun onResponse(response: Response<SettingsQuery.Data>) {
+                        Log.d("MainActivity", response.toString())
+                    }
+
+                    override fun onFailure(e: ApolloException) {
+                        Log.d("MainActivity", e.message.toString())
+                    }
+                }
+            )
+        } catch (e: ApolloException) {
+            Log.d("MainActivity", e.toString())
         }
     }
 }
