d82266f1f8f50e98f425789900996d591457914d
Oleg Yapparov <Oleg.Yapparov@infor.com>
2021-11-18 17:23:24 +0100

    Design login screen, add locale

--- app/src/main/java/com/mlyn/kamenice/LoginActivity.kt
+++ app/src/main/java/com/mlyn/kamenice/LoginActivity.kt
@@ -1,12 +1,11 @@
 package com.mlyn.kamenice
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
 }
